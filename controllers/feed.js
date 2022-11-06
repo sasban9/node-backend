466f16766e2cbd7154cf926bd33d590b2096d7b5
sasban <azure.saswata@gmail.com>
2022-11-06 15:24:53 +0530

    380 Fetching a single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,18 +3,21 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: 1827872,
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "media/duck.jpg",
-        creator: { name: "SasBan" },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      if (!posts) {
+        const error = new Error("No posts found.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Posts fetched.", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -40,6 +43,25 @@ exports.createPost = (req, res, next) => {
         message: "Post created successfully!",
         post: result,
       });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Post fetched.", post: post });
     })
     .catch((err) => {
       if (!err.statusCode) {
