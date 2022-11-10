9e1121c31756b091e16947c3561bbc7090734fea
sasban <azure.saswata@gmail.com>
2022-11-10 15:58:25 +0530

    384 Deleting posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const { validationResult } = require("express-validator");
 
@@ -108,12 +108,12 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
-      if(imageUrl && imageUrl !== post.imageUrl) {
+      if (imageUrl && imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
       post.title = title;
       post.content = content;
-      if(imageUrl)post.imageUrl = imageUrl;
+      if (imageUrl) post.imageUrl = imageUrl;
       return post.save();
     })
     .then((result) => {
@@ -127,7 +127,32 @@ exports.updatePost = (req, res, next) => {
     });
 };
 
-const clearImage = filePath => {
-  filePath = path.join(__dirname, '..', filePath);
-  fs.unlink(filePath, err => console.log(err));
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      //check logged in user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "Deleted Post." });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
