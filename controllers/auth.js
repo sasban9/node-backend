a19b1b47359af795bc037c124425c9fcac426ab0
sasban <azure.saswata@gmail.com>
2022-11-10 21:06:16 +0530

    388 Signing Users up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -13,5 +14,25 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
-  
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPw) => {
+      const user = new User({
+        email: email,
+        name: name,
+        password: hashedPw,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res
+        .status(201)
+        .json({ message: "User created successfully...", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
