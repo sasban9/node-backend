a5324ad9ae1f52553758113eb4ef087ecf5a11c8
sasban <azure.saswata@gmail.com>
2022-11-11 23:19:51 +0530

    391 Logging in & Creating JWTs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -44,19 +45,28 @@ exports.login = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        const error = new Error('A user with this email could not be found.');
+        const error = new Error("A user with this email could not be found.");
         error.statusCode = 401;
         throw error;
       }
       loadedUser = user;
       return bcrypt.compare(password, user.password);
     })
-    .then(isEqual => {
-        if(!isEqual) {
-          const error = new Error('Wrong password!');
-          error.statusCode = 401;
-          throw error;
-        }
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong password!");
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        "somesupersecretsecretsauceSASBAN",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch((err) => {
       if (!err.statusCode) {
