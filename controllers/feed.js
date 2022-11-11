4effc0f8c460747a5271e18d752966804bcbbeac
sasban <azure.saswata@gmail.com>
2022-11-12 00:02:52 +0530

    395 Adding Authorization checks

--- controllers/feed.js
+++ controllers/feed.js
@@ -133,6 +133,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if(post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl && imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -159,6 +164,11 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error("Could not find post.");
         error.statusCode = 404;
+        throw error;
+      }
+      if(post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
         throw error;
       }
       //check logged in user
